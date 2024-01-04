6df9f86eb95127c8348e135c7d23fc6d9f614046
Sang <sangdoxd69@gmail.com>
2024-01-04 11:10:15 +0700

    Fix loi ngu lin

--- src/main/java/com/fa/cybercore/model/DichVu.java
+++ src/main/java/com/fa/cybercore/model/DichVu.java
@@ -25,16 +25,20 @@ public class DichVu {
     @Pattern(regexp = "^DV[0-9]{2}$", message = "Vui lòng nhập đúng định dạng mã dịch vụ. Ví dụ: DV01")
     @Column(name = "MaDV")
     private String maDV;
+
     @NotBlank(message = "Vui lòng nhập tên dịch vụ.")
     @Column(name = "TenDV", columnDefinition = "nvarchar(255)")
     private String tenDV;
+
     @NotBlank(message = "Vui lòng chọn đơn vị tính.")
     @Column(name = "DonViTinh", columnDefinition = "nvarchar(50)")
     private String donViTinh;
+
     @NotNull(message = "Vui lòng nhập đơn giá.")
-    @DecimalMin(value = "1.0", inclusive = false, message = "Đơn giá phải lớn hơn 0")
+    @DecimalMin(value = "0.1", message = "Đơn giá phải lớn hơn 0")
     @Column(name = "DonGia")
     private double donGia;
+
     @OneToMany(mappedBy = "dichVu")
     private List<SuDungDichVu> suDungDichVuList;
 }
